1c265872682e21240dadf24f957caa6880a9794c
meyer <none@none>
2013-08-03 11:38:44 -0500

    initial classification argument parsing

--- classify.py
+++ classify.py
@@ -20,6 +20,7 @@ def classify( infile, data_dir, out_dir, f_fnc = None ):
     '''
     #TODO - verbose as option
     #TODO - k as option
+    #TODO - confusion stats 
 
     #initialize classifier
     nbc = load_classifier( infile )
@@ -53,9 +54,37 @@ def classify( infile, data_dir, out_dir, f_fnc = None ):
             f_fnc( s, d )
 
 if __name__ == '__main__':
-    #TODO argument parsing
-    cfile = 'd1.json'
-    ddir = 'udata'
-    odir = 'cdata'
-    classify( cfile, ddir, odir )
-    #classify( cfile, ddir, odir , f_fnc = cp)
+    def usage():
+        ''' show usage message and exit '''
+        print('usage message goes here')
+        sys.exit(1)
+    import getopt
+    import sys
+    #model_file, data_directory, output_directory
+    # -m --model %a , -d --datadir %a 
+    try:
+        opts, args = getopt.getopt( sys.argv[1:], 'hm:d:',['help','model','datadir'])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+    if 0 != len(args):
+        usage()
+    mfile = None
+    ddir = None
+    odir = None
+    for o,a in opts:
+        if o in ('-h',' --help'):
+            usage()
+        elif o in ('-m', '--model'):
+            mfile = a
+        elif o in ('-d', '--datadir'):
+            ddir = a
+        else:
+            print('unrecognized option %s (argument %s)' % (o,a) )
+            usage()
+    if None == mfile:
+        usage()
+    if None == ddir:
+        usage()
+    classify( mfile, ddir, odir )
+
