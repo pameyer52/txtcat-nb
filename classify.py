bfdf60e428f80ffd86dbec6ebe1a05f1b767b703
meyer <none@none>
2013-08-07 19:54:51 -0500

    removed file copy/move from classification (not that useful)

--- classify.py
+++ classify.py
@@ -5,18 +5,9 @@ import os
 from NaiveBayes import NaiveBayesClassifier
 from nbio import load_words, load_classifier
 
-def cp(src, dst):
-    inp = open(src, 'r')
-    opf = open(dst, 'w')
-    opf.write( inp.read() )
-    inp.close()
-    opf.close()
-
-def classify( infile, data_dir, out_dir, f_fnc = None ):
+def classify( infile, data_dir, out_dir ):
     '''
     assign classification labels to documents using trained classifier.
-    Optionally apply f_fnc to results (to move/copy documents into 
-    subdirectories of out_dir according to assigned label).
     '''
     #TODO - k as option
     #TODO - confusion stats 
@@ -32,25 +23,10 @@ def classify( infile, data_dir, out_dir, f_fnc = None ):
         fws.append( (fn, ws) )
     print('loaded %s files for classification' % len(fws) )
     
-    #setup outputs
-    if None != f_fnc:
-        if os.path.exists( out_dir ):
-            print('output directory %s already exists, aborting' % out_dir)
-            return
-        os.mkdir( out_dir )
-        labels = nbc.get_labels()
-        for l in labels:
-            p = os.path.join( out_dir, l )
-            os.mkdir( p )
-
     #classify
     for (f, ws) in fws:
         l = nbc.classify( ws )
         print('%s classified as %s' % (f, l) )
-        if None != f_fnc:
-            s = os.path.join( data_dir, f )
-            d = os.path.join( out_dir, l, f )
-            f_fnc( s, d )
 
 if __name__ == '__main__':
     def usage():
@@ -59,7 +35,7 @@ if __name__ == '__main__':
         sys.exit(1)
     import getopt
     import sys
-    #model_file, data_directory, output_directory
+    #model_file, data_directory
     # -m --model %a , -d --datadir %a 
     try:
         opts, args = getopt.getopt( sys.argv[1:], 'hm:d:',['help','model=','datadir='])
